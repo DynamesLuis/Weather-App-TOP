36186e9e4b1ee3cbeb2a71c8e469a7edc066c022
Luis Gabriel <luis.gabo.rex@gmail.com>
2026-07-01 14:41:12 -0600

    fix(utils): return correct day phase based on time

--- src/ui/weather/getBgImg.js
+++ src/ui/weather/getBgImg.js
@@ -1,14 +1,28 @@
 function getBgImg(dateTime) {
   const [hourMinute, period] = dateTime.split(" ");
-  const hour = Number(hourMinute.split(":")[0]);
+  let hour = Number(hourMinute.split(":")[0]);
 
-  if (period === "AM") {
-    if (hour >= 5 && hour < 8) return "morning";
+  // Convertir a formato de 24 horas
+  if (period === "PM" && hour !== 12) {
+    hour += 12;
+  }
+
+  if (period === "AM" && hour === 12) {
+    hour = 0;
+  }
+
+  if (hour >= 5 && hour < 8) {
+    return "morning";
+  }
+
+  if (hour >= 8 && hour < 17) {
     return "day";
   }
 
-  // PM
-  if (hour >= 5 && hour < 7) return "sunset";
+  if (hour >= 17 && hour < 19) {
+    return "sunset";
+  }
+
   return "night";
 }
 
